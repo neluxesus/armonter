129fd47ff1af313b2e5a78c2100e703a0a316fde
Armonter <150815976+panteramc@users.noreply.github.com>
2024-03-25 21:11:02 +0100

    Create pterodactyl.php

---
+++ var/www/armonter-client/includes/pterodactyl.php
@@ -0,0 +1,17 @@
+<?php
+// Pterodactyl API functions
+
+function getPterodactylServers() {
+    // Function to retrieve a list of servers from Pterodactyl
+}
+
+function createPterodactylServer($data) {
+    // Function to create a new server on Pterodactyl
+}
+
+function deletePterodactylServer($serverId) {
+    // Function to delete a server on Pterodactyl
+}
+
+// Add more functions as needed to interact with the Pterodactyl API
+?>
