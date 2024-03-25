fc01c9fab5709cb2563cd31238dca256908d27f2
Armonter <150815976+panteramc@users.noreply.github.com>
2024-03-25 21:07:29 +0100

    Create config.php

---
+++ var/www/armonter-client/includes/config.php
@@ -0,0 +1,12 @@
+<?php
+// Database settings
+define('DB_HOST', 'localhost');
+define('DB_USER', 'username');
+define('DB_PASS', 'password');
+define('DB_NAME', 'database');
+
+// Pterodactyl API settings
+define('PTERODACTYL_URL', 'YOUR_PTERODACTYL_URL');
+define('PTERODACTYL_API_KEY', 'YOUR_PTERODACTYL_API_KEY');
+define('PTERODACTYL_ADMIN_API_KEY', 'YOUR_PTERODACTYL_ADMIN_API_KEY');
+?>
