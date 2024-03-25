ac1a3797d9358ea81f70d7381ee60448e63bbd27
Armonter <150815976+panteramc@users.noreply.github.com>
2024-03-25 21:09:35 +0100

    Create functions.php

---
+++ var/www/armonter-client/includes/functions.php
@@ -0,0 +1,17 @@
+<?php
+function isLoggedIn() {
+    // Check if user is logged in
+}
+
+function isDbSetup() {
+    // Check if the database is set up
+}
+
+function updateDbConfig($host, $username, $password, $database) {
+    // Update database configuration in config file
+}
+
+function updatePterodactylConfig($pterodactyl_url, $api_key, $admin_api_key) {
+    // Update Pterodactyl configuration in config file
+}
+?>
